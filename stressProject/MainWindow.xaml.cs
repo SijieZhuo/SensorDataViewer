dcbaddc428e9a09cc26a4eeee84938969adb1a84
Sijie Zhuo <zhuosijie@hotmail.com>
2018-11-27 14:48:52 +1300

    initial update

--- stressProject/MainWindow.xaml.cs
+++ stressProject/MainWindow.xaml.cs
@@ -12,6 +12,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using InTheHand.Net.Sockets;
+using ShimmerAPI;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Enumeration;
+using System.Text.RegularExpressions;
+
+
+
 
 namespace stressProject
 {
@@ -20,9 +28,85 @@ namespace stressProject
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        Dictionary<string, string> BTmap = new Dictionary<string, string>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //BTmap = checkBTConnection();
+
+
+
+
+        }
+
+        /*for (int i = 0; i < 5; i++)
+        {
+            System.Windows.Controls.Button newBtn = new Button();
+
+            newBtn.Content = i.ToString();
+            newBtn.Name = "Button" + i.ToString();
+
+            sp.Children.Add(newBtn);
+        }*/
+
+        public Dictionary<string, string> checkBTConnection()
+        {
+            BluetoothClient client = new BluetoothClient();
+
+
+            BluetoothDeviceInfo[] devices = client.DiscoverDevicesInRange();
+
+            foreach (BluetoothDeviceInfo d in devices)
+            {
+                string address = d.DeviceAddress.ToString();
+                address = Regex.Replace(address, ".{2}", "$0:");
+                address = address.Remove(address.Length - 1);
+
+                BTmap.Add(d.DeviceName, address);
+
+
+                //textBox.AppendText(d.DeviceAddress + "");
+                //textBox.Text.(d.DeviceAddress + "");
+
+                //
+
+
+            }
+
+            return BTmap;
+        }
+
+
+
+
+        protected void button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            textBox.Text = BTmap[button.Content.ToString()];
+        }
+
+        private void BTSearchBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            BTmap = checkBTConnection();
+
+
+            int i = 0;
+            foreach (KeyValuePair<string, string> entry in BTmap)
+            {
+                Button newBtn = new Button();
+
+                newBtn.Content = entry.Key;
+                newBtn.Name = "Button" + i.ToString();
+                i++;
+                sp.Children.Add(newBtn);
+                newBtn.Click += new RoutedEventHandler(button_Click);
+            }
+
         }
     }
+
 }
